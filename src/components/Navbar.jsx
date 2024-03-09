c3c3483229f5dc446e67ebb3a362e084302a3b97
Paul Whittemore <dev@appurist.com>
2024-03-08 23:24:20 -0400

    New logo, added to Navbar (Home) and as favicon (all formats).

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { useLocation, useNavigate, A } from "@solidjs/router";
+import Logo from "~/assets/Logo.png";
 
 export function Navbar() {
   const location = useLocation();
@@ -9,6 +10,7 @@ export function Navbar() {
     <div data-theme="dark" class="navbar navtext flex-none w-100 bg-neutral-950">
       <div class="flex flex-col md:flex-row md:justify-evenly w-full text-lg">
         <A href="/" class={`border-b-2 ${active("/")}`}>
+          <img src={Logo} alt="logo" class="h-8 w-8 me-2 p-1 inline-block" />
           HOME
         </A>
         <A href="/media" class={`border-b-2 ${active("/media")}`}>
