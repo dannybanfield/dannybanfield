504d4e979c22b58eedaa7ec9a58c9e8e4f442cf9
Danny Banfield <banfieldd@gmail.com>
2026-03-02 22:25:12 -0400

    refactor: Update build configs, dependencies, and styling setup

--- src/index.jsx
+++ src/index.jsx
@@ -10,14 +10,12 @@ import { ContactPage } from '~/pages/ContactPage';
 import { NotFoundPage } from '~/pages/404';
 
 render(() => (<>
-  <div class="scroll-smooth">
-    <MetaProvider>
-      <Router>
-        <Route path="/media" component={MediaPage} />
-        <Route path="/contact" component={ContactPage} />
-        <Route path="/" component={HomePage} />
-        <Route path="*404" component={NotFoundPage} />
-      </Router>
-    </MetaProvider>
-  </div>
+  <MetaProvider>
+    <Router>
+      <Route path="/media" component={MediaPage} />
+      <Route path="/contact" component={ContactPage} />
+      <Route path="/" component={HomePage} />
+      <Route path="*" component={NotFoundPage} />
+    </Router>
+  </MetaProvider>
 </>), document.getElementById('root'));
