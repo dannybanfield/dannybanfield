0b52876afb5c1184bf6744801f01b69a119c8c4a
DannyB <banfieldd@gmail.com>
2026-08-18 22:53:34 -0300

    Fixed Darlene typo

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -141,7 +141,7 @@ export function HomePage() {
                       6pm start Nov-May
                     </div>
                     <div class="gradient-text text-lg font-sans">
-                      DM Danny Banfield or Darlen Guite to jam!
+                      DM Danny Banfield or Darlene Guite to jam!
                     </div>
 
                     <div class="flex flex-col lg:flex-row text-left mx-auto mt-3">
